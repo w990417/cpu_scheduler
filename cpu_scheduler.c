2e37d8bb7bca72d3458751dc3999f89fd121d8ec
WonJae <w990417@gmail.com>
2023-05-20 15:56:23 +0900

    Queue and enqueue() complete

--- cpu_scheduler.c
+++ cpu_scheduler.c
@@ -8,38 +8,59 @@
 #define MAX_PROCESS 5
 #define MAX_ARRIVAL_TIME 20
 #define MAX_PRIORITY 4
-#define DEFAULT_PRIORITY 3
+#define DEFAULT_PRIORITY 0
 #define MAX_CPU_BURST 20
 #define DEFAULT_CPU_BURST 10
 #define MAX_IO_BURST 5
 #define DEFAULT_IO_BURST 2
 
+// global clock variable
+int CLK;
 
-// global time variables
-int TICK = 0; // current time
-
-// global struct instances TODO: function to initialise structs with custom/default values
-Config cfg={true, true, true, true, true}; // {rand_pid,  rand_arrival, rand_priority, rand_cpu_burst, rand_io_burst}
-// rand_pid=false is currently not implemented
 
 // FUNCTIONS //
 
 Process* _create_process(Config *cfg){
+    /* 
+    Create a process (struct Process)
+    
+    Parameters
+    ----------
+    Config *cfg: pointer to Config struct which determines whether to randomise process attributes or not
+
+    Returns
+    -------
+    Process *new_process: pointer to new process with attributes...
+    
+    ... pid: (Config.rand_pid=true) 1001 ~ 9999
+             (Config.rand_pid=false) 1 ~ 99 (NOT IMPLEMENTED YET)
+    
+    ... arrival_time: (Config.rand_arrival=true) 0 ~ MAX_ARRIVAL_TIME (20)
+                      (Config.rand_arrival=false) CLK (current time)
+    
+    ... priority: (Config.rand_priority=true) 1 ~ MAX_PRIORITY (4)
+                  (Config.rand_priority=false) DEFAULT_PRIORITY (0: PRIORITY NOT USED)
+    
+    ... cpu_burst_init: (Config.rand_cpu_burst=true) 1 ~ MAX_CPU_BURST (20)
+                        (Config.rand_cpu_burst=false) DEFAULT_CPU_BURST (10)
+
+    ... io_burst_init: (Config.rand_io_burst=true) 1 ~ MAX_IO_BURST (5)
+                       (Config.rand_io_burst=false) DEFAULT_IO_BURST (2)
+
+    ... state: 0=new {1=ready, 2=running, 3=waiting, 4=terminated}
+    
+    ... time related attributes are initialised to -1
+     */
+    
     Process *new_process = (Process*) malloc(sizeof(Process));
     
-    // set pid
-    new_process->pid = rand()%8999 + 1001;   // rand_pid=false is currently not implemented
-    // set arrival time
-    new_process->arrival_time = cfg->rand_arrival ? rand()%MAX_ARRIVAL_TIME + 1 : 0;
-    // set priority
-    new_process->priority = cfg->rand_priority ? rand()%MAX_PRIORITY + 1 : DEFAULT_PRIORITY;
-    // set cpu burst (total)
-    new_process->cpu_burst_init = cfg->rand_cpu_burst ? rand()%MAX_CPU_BURST + 1 : DEFAULT_CPU_BURST;
-    // set I/O burst (total)
-    new_process->io_burst_init = cfg->rand_io_burst ? rand()%MAX_IO_BURST + 1 : DEFAULT_IO_BURST;
-    // set state
-    new_process->state = 0;
 
+    new_process->pid = rand()%8999 + 1001;   // rand_pid=false is currently not implemented
+    new_process->arrival_time = cfg->rand_arrival ? rand()%MAX_ARRIVAL_TIME + 1 : CLK;
+    new_process->priority = cfg->rand_priority ? rand()%MAX_PRIORITY + 1 : DEFAULT_PRIORITY;
+    new_process->cpu_burst_init = cfg->rand_cpu_burst ? rand()%MAX_CPU_BURST + 1 : DEFAULT_CPU_BURST;
+    new_process->io_burst_init = cfg->rand_io_burst ? rand()%MAX_IO_BURST + 1 : DEFAULT_IO_BURST;
+    new_process->state = 0; // new
 
     // time related attributes are initialised to -1
     new_process->ready_wait_time = -1;
@@ -51,8 +72,45 @@ Process* _create_process(Config *cfg){
     return new_process; // return pointer to new process
 }
 
+Queue* create_queue(int priority){
+    /*Create a Queue*/
+    Queue *new_queue = (Queue*)malloc(sizeof(Queue));
+    new_queue->head = NULL;
+    new_queue->tail = NULL;
+    new_queue->priority = priority;
+    new_queue->cnt = 0;
 
-void _enqueue(Queue *q, Process *p, int priority){
+    return new_queue;
+}
+
+
+void enqueue(Queue *q, Process *p){
+    /*
+    Create/allocate a new Node and assign a process to it
+    */
+    
+    // assert q->priority == p->priority
+    if(q->priority != p->priority){
+        printf("Error: Process priority does not match Queue's Priority\n");
+        exit(1);
+    }
+
+    Node *new_node = (Node*)malloc(sizeof(Node));
+    new_node->p = p;
+
+    if(q->head == NULL){
+        q->head = new_node;
+        q->tail = new_node;
+        new_node->left = NULL;
+        new_node->right = NULL;
+    } // queue is empty
+    else{
+        q->tail->right = new_node;
+        new_node->left = q->tail;
+        new_node->right = NULL;
+        q->tail = new_node;
+    } // queue is not empty
+    q->cnt++;
 
 }
 
@@ -88,19 +146,61 @@ void print_process_info(Process* p){
 }
 
 
+void print_queue(Queue *q){
+
+    // check if empty
+    if(q->head == NULL){
+        printf("Queue is empty\n");
+        return;
+    }
+
+    // print queue info
+    printf("Queue Priority: %d\n", q->priority);
+    printf("Processes in queue: %d\n", q->cnt);
+    
+    // print queue
+    Node *curr = q->head;
+    while (curr != NULL) {
+        printf("[%d] -->", curr->p->pid);
+        curr = curr->right;
+    }
+    printf("NULL\n");
+}
+
+
 int main(){
+    
+    
+    // set test cfg
+    Config cfg = {
+        .rand_pid = true,
+        .rand_arrival = true,
+        .rand_priority = false,
+        .rand_cpu_burst = true,
+        .rand_io_burst = true
+    };
+    CLK = 0;
     srand(99);
     
+    // job pool is a simple array of pointers to processes for now
+    Process* job_pool[MAX_PROCESS];
     int p_cnt = 0; // number of processes in job pool
 
-    Process* job_pool[MAX_PROCESS];
-
+    // create processess
     for(int i=0; i<MAX_PROCESS; i++){
         job_pool[i] = _create_process(&cfg);
         print_process_info(job_pool[i]);
         p_cnt++;
     }
 
+    // queue test
+    Queue* test_queue = create_queue(0);
+    for(int i=0; i<MAX_PROCESS; i++){
+        enqueue(test_queue, job_pool[i]);
+    }
+
+    // print queue
+    print_queue(test_queue);
 
 
 
--- cpu_scheduler.h
+++ cpu_scheduler.h
@@ -21,7 +21,7 @@ Config: keeps track of current configuration
 
 
 // structs
-typedef struct {
+typedef struct Process{
     // initial data
     int pid;             // 1001 ~ 9999
     int state;           // 0=new, 1=ready, 2=running, 3=waiting, 4=terminated
@@ -39,36 +39,16 @@ typedef struct {
 }Process;
 
 
-typedef struct{
-    /*
-    Priority Queue: each entry is a queue of processes of the same priority (i.e. linked list)
-     */
-    // queue of processes of priority 1
-    struct Node *p1;
-    // queue of processes of priority 2
-    struct Node *p2;
-    // queue of processes of priority 3
-    struct Node *p3;
-    // queue of processes of priority 4
-    struct Node *p4;
-
-    // processes count
-    int p_cnt;
-    int p1_cnt;
-    int p2_cnt;
-    int p3_cnt;
-    int p4_cnt;
-
-}PQueue;
-
-typedef struct{
+typedef struct Queue{
     /* One dimensional queue (linked list) with no priority*/
     struct Node *head;
-    int p_cnt;
+    struct Node *tail;
+    int priority;       // 1~MAX_PRIORITY where 1 is the highest. If priority=0, then it is not used.
+    int cnt;
 }Queue;
 
 
-typedef struct{
+typedef struct Node{
     /*
     Queue (linked list) of processes of the same priority
 
@@ -81,24 +61,25 @@ typedef struct{
 }Node;
 
 
-typedef struct{
+typedef struct Config{
     bool rand_pid;      // false: (default) increments from 1001
                         // true: random (1001 ~ 9999)
     bool rand_arrival;  // false: (default) increments from 0
                         // true: random (0 ~ MAX_ARRIVAL_TIME)
-    bool rand_priority; // false: (default) priority is fixed to DEFAULT_PRIORITY for all processes
-                        // true: priority is random (0 ~ MAX_PRIORITY)
+    bool rand_priority; // false: (default) set to DEFAULT_PRIORITY for all processes (0: PRIORITY NOT USED)
+                        // true: random (1 ~ MAX_PRIORITY)
     bool rand_cpu_burst;// false: (default) fixed to DEFAULT_CPU_BURST for all processes
                         // true: random (1 ~ MAX_CPU_BURST)
     bool rand_io_burst; // false: (default) fixed to DEFAULT_IO_BURST for all processes                   
 }Config;
 
 // function prototypes
-Queue* create_queue(int priority);
-// Process* create_process();
 Process* _create_process(Config *cfg);
+Queue* create_queue(int priority);
+void enqueue(Queue *q, Process *p);
 void print_process_info(Process *p);
-void _enqueue();
+void print_queue(Queue *q);
+
 
 
 #endif  // CPU_SCHEDULER_H
